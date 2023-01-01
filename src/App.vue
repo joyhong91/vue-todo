337fa467ee6fe3debeadfcad1be0e509d82e7828
joy hong <remonkp@gmail.com>
2023-01-01 16:47:49 +0900

    [vuex] 설치 및 등록

--- src/App.vue
+++ src/App.vue
@@ -79,10 +79,10 @@ export default {
     }
   },
   components: {
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 
 }
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+//Vue를 사용할때 글로벌 펑션을 추가할때 use를 쓴다.
+Vue.use(Vuex);
+
+export const store = new Vuex.store({
+
+});
